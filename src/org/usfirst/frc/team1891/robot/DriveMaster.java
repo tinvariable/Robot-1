affdccba951d2b1e77a2fd6df61e119b63052d87
Tyler <sylsr9@gmail.com>
2015-01-17 09:14:17 -0700

    DriveMaster class
    DriveMaster class is working correctly and calling into the robot class

--- src/org/usfirst/frc/team1891/robot/DriveMaster.java
+++ src/org/usfirst/frc/team1891/robot/DriveMaster.java
@@ -1,27 +1,50 @@
 package org.usfirst.frc.team1891.robot;
 
 import edu.wpi.first.wpilibj.CANJaguar;
-import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 
-public class DriveMaster extends IterativeRobot
+public class DriveMaster
 {
-	//instantiate all jags with correct ID numbers
-	CANJaguar jag3 = new CANJaguar(3);
-	CANJaguar jag4 = new CANJaguar(4);
-	CANJaguar jag5 = new CANJaguar(5);
-	CANJaguar jag6 = new CANJaguar(6);
-	//Extremem3DPro
-	Joystick joyRight = new Joystick(1);
-	//Joystick constructer will have to be changed when port number is reassigned
-	//ATK3
-	Joystick joyLeft = new Joystick(0);
-	//retrieve the joystick axis
-	public void basicMove()
+	Joystick joyRight;
+	final double DEADZONE=0.05;
+	public DriveMaster(int joyPort)
 	{
-		jag3.set(joyRight.getY());
-		jag6.set(joyRight.getY());
-		jag4.set(-(joyLeft.getY()));
-		jag5.set(-(joyLeft.getY()));
+		joyRight= new Joystick(joyPort);
 	}
+	public double getYAxis()
+	{
+		double value = joyRight.getY();
+		if(Math.abs(value) > DEADZONE)
+		{
+			return value;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	public double getXAxis()
+	{
+		double value = joyRight.getY();
+		if(Math.abs(value) > DEADZONE)
+		{
+			return value;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	public double getZAxis()
+	{
+		double value = joyRight.getY();
+		if(Math.abs(value) > DEADZONE)
+		{
+			return value;
+		}
+		else
+		{
+			return 0;
+		}
+	}	
 }
--- src/org/usfirst/frc/team1891/robot/Robot.java
+++ src/org/usfirst/frc/team1891/robot/Robot.java
@@ -4,11 +4,12 @@ package org.usfirst.frc.team1891.robot;
 import edu.wpi.first.wpilibj.CANJaguar;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
+import org.omg.CORBA.portable.UnknownException;
 import org.usfirst.frc.team1891.robot.commands.ExampleCommand;
 import org.usfirst.frc.team1891.robot.subsystems.ExampleSubsystem;
 
@@ -17,19 +18,22 @@ public class Robot extends IterativeRobot
 
 	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
 	public static OI oi;
-	DriveMaster testDrive = new DriveMaster();
-
+	DriveMaster roboDrive;
+	Command autonomousCommand;
+    CANJaguar jag3;
+    CANJaguar jag4;
+    CANJaguar jag5;
+    CANJaguar jag6;
+    
 	
-	
-    Command autonomousCommand;
     public void robotInit()
     {
 		oi = new OI();
-        autonomousCommand = new ExampleCommand();
-        
-        
-
-
+		jag3 = new CANJaguar(3);
+		jag4 = new CANJaguar(4);
+		jag5 = new CANJaguar(5);
+		jag6 = new CANJaguar(6);
+		roboDrive = new DriveMaster(1);
     }
 	
 	public void disabledPeriodic() 
@@ -58,17 +62,22 @@ public class Robot extends IterativeRobot
         {
         	autonomousCommand.cancel();
         }
+        
      }   	
         	
-    public void disabledInit(){
+    public void disabledInit()
+    {
 
     }
 
     public void teleopPeriodic()
     {	
 		Scheduler.getInstance().run();
-		testDrive.basicMove();
-        
+
+		jag3.set(roboDrive.getYAxis());
+		jag4.set(roboDrive.getYAxis());
+		jag5.set(roboDrive.getYAxis());
+		jag6.set(roboDrive.getYAxis());
     }
     
 
